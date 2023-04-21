3fbb39018f70a24eca94aa910ebeccf01b2b25e3
wajahat8888 <97318378+wajahat8888@users.noreply.github.com>
2023-04-21 21:51:59 +0500

    firt push

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+// require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
+
+const QUICKNODE_RPC_URL = process.env.QUICKNODE_RPC_URL;
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: QUICKNODE_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
